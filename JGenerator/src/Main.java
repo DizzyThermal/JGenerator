6441059ae02f92cfe4598e5f46d251d23704347f
asimmons <lamborghini217@gmail.com>
2013-03-12 00:03:07 -0400

    Original Code

--- JGenerator/src/Main.java
+++ JGenerator/src/Main.java
@@ -1,7 +1,19 @@
-public class Main
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.net.URLConnection;
+
+
+public class Main 
 {
-	public static void main(String[] args)
+
+	public static void main(String[] args) throws IOException 
 	{
+		String urlString = "http://192.168.1.100:8080";
+		URL url = new URL(urlString);
+		URLConnection conn = url.openConnection();
+		InputStream is = conn.getInputStream();
 
 	}
+
 }
