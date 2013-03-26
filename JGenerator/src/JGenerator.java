1a08cdc5a241b4c9b5ce31a62c7ad894867f51ec
Stephen Diniz <DizzyThermal@gmail.com>
2013-03-25 21:17:56 -0400

    Threading?

--- JGenerator/src/JGenerator.java
+++ JGenerator/src/JGenerator.java
@@ -25,6 +25,12 @@ public class JGenerator
 	
 	private static boolean GUI = false;
 	
+	private static int t1Request = 0;
+	private static int t2Request = 0;
+	
+	private static double t1TotalTime = 0;
+	private static double t2TotalTime = 0;
+	
 	public static void main(String[] args) throws IOException 
 	{
 		if(args.length < 2 || args.length > 4)
@@ -42,7 +48,8 @@ public class JGenerator
 	public static String generateTraffic() throws IOException
 	{
 		int requestCount = 0;
-		double start = 0, finish = 0, totalTime = 0;
+		double start = 0, finish = 0;
+		double totalTime = 0;
 		BufferedReader in = null;
 		BufferedReader other = null;
 		HttpURLConnection conn = null;
@@ -77,26 +84,108 @@ public class JGenerator
 				conn.disconnect();
 				break;
 			case NPP:
-				while(totalTime/1000 < DURATION)
+				boolean t1Done = false;
+				boolean t2Done = false;
+
+				Thread t1 = (new Thread()
 				{
-					requestCount+=2;
-					start = System.currentTimeMillis();
-					String urlString = URL;
-					URL url = new URL(urlString);
-					conn = (HttpURLConnection) url.openConnection();
-					conn.setRequestProperty("Connection", "close");
-					pic = (HttpURLConnection) new URL(urlString + "/0.jpg").openConnection();
-					pic.setRequestProperty("Connection", "close");
-					in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-					other = new BufferedReader(new InputStreamReader(pic.getInputStream()));
-					while(in.read() != -1 && other.read() != -1);
-					while((2000/rate) > System.currentTimeMillis()-start);
-					totalTime += System.currentTimeMillis() - start;
-					in.close();
-					other.close();
-				}
-				conn.disconnect();
-				pic.disconnect();
+					@Override
+					public void run()
+					{
+						try
+						{
+							int requestCount = 0;
+							double start = 0, finish = 0;
+							double totalTime = 0;
+							BufferedReader in = null;
+							BufferedReader other = null;
+							HttpURLConnection conn = null;
+							HttpURLConnection pic = null;
+							
+							while(totalTime/1000 < DURATION)
+							{
+								requestCount += 2;
+								start = System.currentTimeMillis();
+								URL url = new URL(URL);
+								conn = (HttpURLConnection) url.openConnection();
+								conn.setRequestProperty("Connection", "close");
+								pic = (HttpURLConnection) new URL(URL + "/0.jpg").openConnection();
+								pic.setRequestProperty("Connection", "close");
+								in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+								other = new BufferedReader(new InputStreamReader(pic.getInputStream()));
+								
+								while(in.read() != -1 && other.read() != -1)
+									;
+								
+								while((2000/(rate/2)) > System.currentTimeMillis()-start)
+									;
+								
+								totalTime += System.currentTimeMillis() - start;
+								in.close();
+								other.close();
+							}
+							
+							conn.disconnect();
+							pic.disconnect();
+							setThreadValue(1, requestCount, totalTime);
+						}
+						catch(IOException ioe) { ioe.printStackTrace(); }
+					}
+				});
+				
+				Thread t2 = (new Thread()
+				{
+					@Override
+					public void run()
+					{
+						try
+						{
+							int requestCount = 0;
+							double start = 0, finish = 0;
+							double totalTime = 0;
+							BufferedReader in = null;
+							BufferedReader other = null;
+							HttpURLConnection conn = null;
+							HttpURLConnection pic = null;
+							
+							while(totalTime/1000 < DURATION)
+							{
+								requestCount += 2;
+								start = System.currentTimeMillis();
+								URL url = new URL(URL);
+								conn = (HttpURLConnection) url.openConnection();
+								conn.setRequestProperty("Connection", "close");
+								pic = (HttpURLConnection) new URL(URL + "/0.jpg").openConnection();
+								pic.setRequestProperty("Connection", "close");
+								in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+								other = new BufferedReader(new InputStreamReader(pic.getInputStream()));
+								
+								while(in.read() != -1 && other.read() != -1)
+									;
+								
+								while((2000/(rate/2)) > System.currentTimeMillis()-start)
+									;
+								
+								totalTime += System.currentTimeMillis() - start;
+								in.close();
+								other.close();
+							}
+							
+							conn.disconnect();
+							pic.disconnect();
+							setThreadValue(2, requestCount, totalTime);
+						}
+						catch(IOException ioe) { ioe.printStackTrace(); }
+					}
+				});
+
+				start = System.currentTimeMillis();
+				t1.start();
+				t2.start();
+				while(t1.isAlive() || t2.isAlive())
+					;
+				totalTime = (t1TotalTime > t2TotalTime)?t1TotalTime:t2TotalTime;
+				requestCount = t1Request + t2Request;
 				break;
 			default:
 				while(totalTime/1000 < DURATION)
@@ -219,5 +308,20 @@ public class JGenerator
 	public static double round(Double value)
 	{
 		return Double.valueOf(new DecimalFormat("#.###").format(value));
+	}
+	
+	public static void setThreadValue(int thread, int requestTime, double totalTime)
+	{
+		switch(thread)
+		{
+			case 1:
+				t1Request = requestTime;
+				t1TotalTime = totalTime;
+				break;
+			case 2:
+				t2Request = requestTime;
+				t2TotalTime = totalTime;
+				break;
+		}
 	}
 }
